b131abdb2921af715adf9d2cf34439d7be4f4241
Maurice van Cooten <maurice@minus.nz>
2017-09-22 09:21:22 +1200

    component example

---
+++ component/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/hello-world'
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById('content')
+)
