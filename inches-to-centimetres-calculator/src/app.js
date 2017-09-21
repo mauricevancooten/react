31fc99d384b59b64950ad662e006576c012700e6
Maurice van Cooten <maurice@minus.nz>
2017-09-22 09:23:07 +1200

    metric conversion using state

---
+++ inches-to-centimetres-calculator/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import InchesToCentimetres from './components/inches-to-centimetres'
+
+ReactDOM.render(
+  <InchesToCentimetres />,
+  document.getElementById('content')
+)
---
+++ inches-to-centimetres-calculator/src/components/inches-to-centimetres.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class InchesToCentimetres extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {inches:''}
+    this.inchesChanged = this.inchesChanged.bind(this)
+    this.resetInches = this.resetInches.bind(this)
+  }
+  inchesChanged(event) {
+    let centimetres = event.target.value  / .39370079
+    centimetres = Math.round(centimetres * 10) / 10
+    this.setState({inches: centimetres })
+  }
+  resetInches(event) {
+    this.setState({inches: ''})
+  }
+  render() {
+    // console.log(this.state)
+    return (
+      <div>
+        <p>Inches: <input type='text' onChange={this.inchesChanged}/></p>
+        <h2>Centimetres: {this.state.inches} </h2>
+        <button onClick={this.resetInches}>Reset</button>
+      </div>
+    )
+  }
+}
+
+export default InchesToCentimetres
