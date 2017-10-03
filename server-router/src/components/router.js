bc6c047a4a0dd73732ee3ddcdeeaf3973739bf36
Maurice van Cooten <maurice@minus.nz>
2017-10-03 21:57:09 +1300

    new npm version and shorthand syntax literal

--- server-router/src/components/router.js
+++ server-router/src/components/router.js
@@ -1,34 +1,34 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import About from './about'
 
-class Router extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>Site Name</h1>
-          <nav>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/about'>About</Link></li>
-            </ul>
-          </nav>
-        </header>
-        <div className='wrapper'>
-          <div className='container'>
-            <Route exact path='/' render={() => <h1>Home</h1>}/>
-            <Route path='/about' component={About}/>
-          </div> {/* .container */}
-        </div> {/* .wrapper */}
-        <footer>
-          <small>
-            <a href='https://opensource.org/licenses/MIT'>MIT</a>
-          </small>
-        </footer>
+const Router = () => (
+  <div>
+    <header>
+      <h1>Site Name</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div className='wrapper'>
+      <div className='container'>
+        <Route exact path='/' render={() => <h1>Home</h1>}/>
+        <Route path='/about' component={About}/>
       </div>
-    )
-  }
-}
+    </div>
+    <footer>
+      <small>
+        <a href='https://opensource.org/licenses/MIT'>MIT</a>
+      </small>
+    </footer>
+  </div>
+)
 
 export default Router
