83055c4d123f45601581023340b430952c379507
Maurice van Cooten <maurice@minus.nz>
2017-10-20 11:31:50 +1300

    With SSR corrected, context.status can be used for 404

--- data/src/index.js
+++ data/src/index.js
@@ -10,11 +10,6 @@ const app = express()
 
 app.use('/static', express.static('public'))
 
-const routes = ['/', '/about']
-data.artists.map((artist) => {
-  routes.push(`/${artist.handle}`)
-})
-
 app.get('*', (req, res) => {
   const context = {}
 
@@ -24,20 +19,12 @@ app.get('*', (req, res) => {
     </StaticRouter>
   )
 
-  const match = routes.reduce((acc, route) => matchPath(req.url, route, {exact: true}) || acc, null)
-
-  if (!match.isExact) {
-    res.status(404).send(Template({
-      html: html,
-      // data: data.artists
-    }))
-    return
+  if (context.status) {
+    res.status(404).send(Template({html: html}))
+  } else {
+    res.status(200).send(Template({html: html, data: data.artists}))
   }
 
-  res.status(200).send(Template({
-    html: html,
-    data: data.artists
-  }))
 })
 
 app.listen(3000, () => {
