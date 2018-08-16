f62e6da6ae1a24272f58786ca3a05103a8029306
Maurice van Cooten <maurice@kosmo.studio>
2018-08-16 19:35:09 +1200

    Webpack 4

--- router-dom/webpack.config.js
+++ router-dom/webpack.config.js
@@ -1,17 +1,18 @@
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
     path: __dirname + '/public/js/',
     filename: 'bundle.js'
   },
-  devtool: 'sourcemap',
+  devtool: 'source-map',
   module: {
     rules: [
-       {
-         test: /\.js?$/,
-         exclude: /(node_modules)/,
-         loader: 'babel-loader'
-       }
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
     ]
   }
 }
